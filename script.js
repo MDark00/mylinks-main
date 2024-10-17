9ae154b01acbf4402121e6ca98b09fa8f079d158
karam_khalil <karamgamar2008@gmail.com>
2024-10-17 10:03:36 +0300

    second commit

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 function createSnowflake() {
-    let randomani = Math.floor(Math.random() * 3 + 8) ;
-    const snowflake = document.createElement('i');
-    snowflake.classList.add('fas', 'fa-code', 'snowflake'); 
-    snowflake.style.left = Math.random() * window.innerWidth + 'px'; 
-    snowflake.style.animationDuration = randomani + 's'; 
-    snowflake.style.opacity = Math.random(); 
-    snowflake.style.fontSize = Math.random() * 10 + 10 + 'px'; 
-    document.body.appendChild(snowflake);
+  let randomani = Math.floor(Math.random() * 3 + 8);
+  const snowflake = document.createElement("i");
+  snowflake.classList.add("fas", "fa-code", "snowflake");
+  snowflake.style.left = Math.random() * window.innerWidth + "px";
+  snowflake.style.animationDuration = randomani + "s";
+  snowflake.style.opacity = Math.random();
+  snowflake.style.fontSize = Math.random() * 10 + 10 + "px";
+  document.body.appendChild(snowflake);
 
-
-    setTimeout(() => {
-        snowflake.remove();
-    }, randomani * 1000  ); 
-
-
+  setTimeout(() => {
+    snowflake.remove();
+  }, randomani * 1000);
 }
 
 createSnowflake();
-setInterval(createSnowflake, 1000); 
+setInterval(createSnowflake, 1000);
